d0fbdb3c4bf3c1246d61df19227e0a9b0395821f
saimun5656 <shaounlosted@gmail.com>
2023-05-20 11:57:38 +0600

    context provider created with firebase methods

---
+++ src/Shared/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth" 
+import app from "../../Firebase/firebase.config";
+export const AuthContext = createContext(null);
+const auth =getAuth(app)
+
+const AuthProvider = ({children}) => {
+const googleProvider = new GoogleAuthProvider();
+const [user,setUser] = useState({});
+const [loading,setLoading] = useState(true);
+
+useEffect(()=>{
+  const unSub = onAuthStateChanged(auth,(currentUser)=>{
+        setUser(currentUser)
+        setLoading(false)
+      })
+  return ()=>unSub()
+},[])
+
+const googleSignIn=()=>{
+   return signInWithPopup(auth,googleProvider)
+}
+const createUser = ()=>{
+     return createUserWithEmailAndPassword()
+}
+const login = (email,password)=>{
+   return signInWithEmailAndPassword(auth,email,password);
+}
+const logOut =()=>{
+  return signOut(auth);
+}
+const authInfo = {
+    user,
+    loading,
+    googleSignIn,
+    createUser,
+    login,
+    logOut
+}
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
